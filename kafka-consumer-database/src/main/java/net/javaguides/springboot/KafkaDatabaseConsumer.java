f80e6e260f3e09b568f5aeb2e7357ff32f5c232e
Fili <fili@Filis-MacBook-Pro.local>
2023-06-21 16:54:07 -0500

    json ripper

--- kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -4,18 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
 public class KafkaDatabaseConsumer {
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
 
-//    private WikimediaDataRepo wikimediaDataRepo;
-//    public KafkaDatabaseConsumer(WikimediaDataRepo wikimediaDataRepo){
-//        this.wikimediaDataRepo = wikimediaDataRepo;
-//    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
@@ -23,53 +23,51 @@ public class KafkaDatabaseConsumer {
             topics = "RTDIS_1",
             groupId = "myGroup")
     public void consume(String eventMessage)  {
-        eventMessage = eventMessage.substring(1, eventMessage.length() - 1);
-        eventMessage = eventMessage.replaceAll("\\\\", "");
         LOGGER.info(String.format("Event message received -> %s",eventMessage));
-        try {
-// Parse the JSON string
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = eventMessage;
-            JsonNode rootNode = objectMapper.readTree(json);
-            System.out.println();
-// Extract specific data fields
-            String topic = rootNode.path("meta").path("url").asText();
-            System.out.println("topic = " + topic);
-        } catch (IOException e) {
-            e.printStackTrace();
+        String newTopic = parseTopic(eventMessage);
+        if (newTopic != null) {
+            pushToKafka(newTopic, eventMessage);
         }
-
 //        WikimediaData wikimediaData = new WikimediaData();
 //        wikimediaData.setWikiEventData(eventMessage);
 //        wikimediaDataRepo.save(wikimediaData);
     }
 
-    public void pushToKafka() {
-        /*
-        String createTopic = "wikimedia-create";
-        String changeTopic = "wikimedia-update";
-        String deleteTopic = "wikimedia-delete";
+    public void pushToKafka(String topicName, String message) {
+        kafkaTemplate.send(topicName, message);
+    }
 
-        EventHandler createEventHandler = new WikimediaChangesHandler(kafkaTemplate,createTopic);
-        String createUrl = "https://stream.wikimedia.org/v2/stream/page-create";
+    public String parseTopic(String eventMessage) {
+        int startIndex = eventMessage.indexOf("mediawiki");
 
-        EventHandler updateEventHandler = new WikimediaChangesHandler(kafkaTemplate,changeTopic);
-        String changeUrl = "https://stream.wikimedia.org/v2/stream/page-links-change";
+        if (startIndex != -1) {
+            // Find the end index of the closing double quote
+            int endIndex = eventMessage.indexOf("\"", startIndex + 1);
 
-        EventHandler deleteEventHandler = new WikimediaChangesHandler(kafkaTemplate,deleteTopic);
-        String deleteUrl = "https://stream.wikimedia.org/v2/stream/page-delete";
+            if (endIndex != -1) {
+                // Extract the desired substring
+                String extractedSubstring = eventMessage.substring(startIndex, endIndex);
+                System.out.println("Extracted substring: " + extractedSubstring);
+                // if statement to check if extractedSubstring contains create, delete, change substring
+                if (extractedSubstring.contains("create")) {
+                    return "DS_create";
+                } else if (extractedSubstring.contains("delete")) {
+                    return "DS_delete";
+                } else if (extractedSubstring.contains("change")) {
+                    return "DS_update";
+                } else {
+                    System.out.println("Substring 'create', 'delete', or 'change' not found.");
+                    return null;
+                }
 
-        EventSource.Builder createBuilder = new EventSource.Builder(createEventHandler, URI.create(createUrl));
-        EventSource.Builder changeBuilder = new EventSource.Builder(updateEventHandler, URI.create(changeUrl));
-        EventSource.Builder deleteBuilder = new EventSource.Builder(deleteEventHandler, URI.create(deleteUrl));
+            } else {
+                System.out.println("Closing double quote not found.");
+                return null;
+            }
+        } else {
+            System.out.println("Substring 'mediawiki' not found.");
+            return null;
+        }
 
-        EventSource createEventSource = createBuilder.build();
-        EventSource changeEventSource = changeBuilder.build();
-        EventSource deleteEventSource = deleteBuilder.build();
-
-        createEventSource.start();
-        changeEventSource.start();
-        deleteEventSource.start();
-        * */
     }
 }
