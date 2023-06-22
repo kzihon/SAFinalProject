835d367b7d0a9f2c5a4f8e8cc4fb451730cded74
Fili <fili@Filis-MacBook-Pro.local>
2023-06-21 20:44:43 -0500

    pre presentation

--- kafka-consumer-create/src/main/java/edu/miu/saproject/KafkaCreateConsumer.java
+++ kafka-consumer-create/src/main/java/edu/miu/saproject/KafkaCreateConsumer.java
@@ -1,10 +1,24 @@
 package edu.miu.saproject;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.opencsv.CSVWriter;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class KafkaCreateConsumer {
@@ -15,16 +29,92 @@ public class KafkaCreateConsumer {
 //    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaCreateConsumer.class);
+    List<Object> eventData = new ArrayList<>();
+    String filePath = "/Users/fili/Desktop/SAFinalProjectd/wikipdiaCsv/output.json";
+    JSONObject jsonData = new JSONObject();
 
     @KafkaListener(
-            topics = "wikimedia-create",
+            topics = "DS_create",
             groupId = "myGroup")
-    public void consume(String eventMessage){
+    public void consume(String eventMessage) throws IOException {
 
-        LOGGER.info(String.format("Event message received -> %s",eventMessage));
+        LOGGER.info(String.format("Event message received -> %s", eventMessage));
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter("create-output.csv", true))) {
+            // Write data rows
+            String[] row1 = parseTopic(eventMessage);
+            csvWriter.writeNext(row1);
+            System.out.println("CSV file was updated successfully !!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-//        WikimediaData wikimediaData = new WikimediaData();
-//        wikimediaData.setWikiEventData(eventMessage);
-//        wikimediaDataRepo.save(wikimediaData);
+    public String[] parseTopic(String eventMessage) {
+        String[] row = new String[5];
+        // add a formatted timestamp to the first column
+        LocalDateTime timestamp = LocalDateTime.now();
+
+        // Define the desired format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // Format the timestamp as a string
+        String timestampString = timestamp.format(formatter);
+        row[0] = timestampString;
+        int startIndex = eventMessage.indexOf("topic");
+
+        if (startIndex != -1) {
+            // Find the end index of the closing double quote
+            int endIndex = eventMessage.indexOf("\\", startIndex + 10);
+
+            if (endIndex != -1) {
+                // Extract the desired substring
+                String extractedSubstring = eventMessage.substring(startIndex+10, endIndex);
+                row[1] = extractedSubstring;
+                // if statement to check if extractedSubstring contains create, delete, change substring
+            }
+        }
+
+        startIndex = eventMessage.indexOf("https");
+
+        if (startIndex != -1) {
+            // Find the end index of the closing double quote
+            int endIndex = eventMessage.indexOf("\"", startIndex + 1);
+
+            if (endIndex != -1) {
+                // Extract the desired substring
+                String extractedSubstring = eventMessage.substring(startIndex, endIndex-1);
+                row[2] = extractedSubstring;
+                // if statement to check if extractedSubstring contains create, delete, change substring
+            }
+        }
+        startIndex = eventMessage.indexOf("user_text");
+
+        if (startIndex != -1) {
+            // Find the end index of the closing double quote
+            int endIndex = eventMessage.indexOf("\"", startIndex + 14);
+
+            if (endIndex != -1) {
+                // Extract the desired substring
+                String extractedSubstring = eventMessage.substring(startIndex+14, endIndex-1);
+                row[3] = extractedSubstring;
+                // if statement to check if extractedSubstring contains create, delete, change substring
+            }
+        }
+        startIndex = eventMessage.indexOf("domain");
+
+        if (startIndex != -1) {
+            // Find the end index of the closing double quote
+            int endIndex = eventMessage.indexOf("\"", startIndex + 11);
+
+            if (endIndex != -1) {
+                // Extract the desired substring
+                String extractedSubstring = eventMessage.substring(startIndex+11, endIndex-1);
+                row[4] = extractedSubstring;
+                // if statement to check if extractedSubstring contains create, delete, change substring
+            }
+        }
+        System.out.println(row[0] + " " + row[1] + " " + row[2]);
+        return row;
+
     }
 }
--- kafka-consumer-update/src/main/java/edu/miu/saproject/KafkaUpdateConsumer.java
+++ kafka-consumer-update/src/main/java/edu/miu/saproject/KafkaUpdateConsumer.java
@@ -1,30 +1,73 @@
 package edu.miu.saproject;
 
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class KafkaUpdateConsumer {
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
 
-//    private WikimediaDataRepo wikimediaDataRepo;
-//    public KafkaDatabaseConsumer(WikimediaDataRepo wikimediaDataRepo){
-//        this.wikimediaDataRepo = wikimediaDataRepo;
-//    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaUpdateConsumer.class);
 
     @KafkaListener(
-            topics = "wikimedia-update",
+            topics = "RTDIS_1",
             groupId = "myGroup")
-    public void consume(String eventMessage){
-
+    public void consume(String eventMessage)  {
         LOGGER.info(String.format("Event message received -> %s",eventMessage));
-
+        String newTopic = parseTopic(eventMessage);
+        if (newTopic != null) {
+            pushToKafka(newTopic, eventMessage);
+        }
 //        WikimediaData wikimediaData = new WikimediaData();
 //        wikimediaData.setWikiEventData(eventMessage);
 //        wikimediaDataRepo.save(wikimediaData);
+    }
+
+    public void pushToKafka(String topicName, String message) {
+        kafkaTemplate.send(topicName, message);
+    }
+
+    public String parseTopic(String eventMessage) {
+        int startIndex = eventMessage.indexOf("mediawiki");
+
+        if (startIndex != -1) {
+            // Find the end index of the closing double quote
+            int endIndex = eventMessage.indexOf("\"", startIndex + 1);
+
+            if (endIndex != -1) {
+                // Extract the desired substring
+                String extractedSubstring = eventMessage.substring(startIndex, endIndex);
+                System.out.println("Extracted substring: " + extractedSubstring);
+                // if statement to check if extractedSubstring contains create, delete, change substring
+                if (extractedSubstring.contains("create")) {
+                    return "DS_create";
+                } else if (extractedSubstring.contains("delete")) {
+                    return "DS_delete";
+                } else if (extractedSubstring.contains("change")) {
+                    return "DS_update";
+                } else {
+                    System.out.println("Substring 'create', 'delete', or 'change' not found.");
+                    return null;
+                }
+
+            } else {
+                System.out.println("Closing double quote not found.");
+                return null;
+            }
+        } else {
+            System.out.println("Substring 'mediawiki' not found.");
+            return null;
+        }
+
     }
 }
